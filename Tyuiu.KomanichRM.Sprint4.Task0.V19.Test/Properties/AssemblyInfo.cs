da916ba4d81fd75a961e69e8cb0ed43166443682
Tyuiu.KomanichRM@outlook.com <gorotro_215@mail.ru>
2023-10-30 17:27:56 +0500

    Add project files.

---
+++ Tyuiu.KomanichRM.Sprint4.Task0.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomanichRM.Sprint4.Task0.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomanichRM.Sprint4.Task0.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("727e3547-36c3-44a6-9032-074d07360f90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KomanichRM.Sprint4.Task0.V19/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomanichRM.Sprint4.Task0.V19.Lib;
+
+namespace Tyuiu.KomanichRM.Sprint4.Task0.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Команич Р. М. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Команич Роман Маркович | АСОиУб-23-3                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9                           *");
+            Console.WriteLine("* подсчитать произведение нечетных элементов массива.                     *");
+            Console.WriteLine("* {9, 5, 7, 4, 5, 3, 7, 8, 9, 1}                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[] numsArray = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i <= numsArray.Length - 1; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.GetMultOddArrEl(numsArray);
+            Console.WriteLine("Произведение нечетных элементов массива равно = " + res);
+            Console.ReadKey();
+        }
+    }
+}

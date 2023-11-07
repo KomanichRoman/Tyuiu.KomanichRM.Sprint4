723adadbc0f92c4935a5fae8109aa04e610e2100
Tyuiu.KomanichRM@outlook.com <gorotro_215@mail.ru>
2023-11-07 21:13:07 +0500

    Task4

---
+++ Tyuiu.KomanichRM.Sprint4.Task4.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomanichRM.Sprint4.Task4.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomanichRM.Sprint4.Task4.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("57d5d824-fafb-4224-a3ea-991b1a0b5dbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
